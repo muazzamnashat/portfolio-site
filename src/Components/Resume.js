49d8166481526f92b60b3b6e32c5fe981924e0f9
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-03-07 22:47:58 -0500

    add eduction and work data in json

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -13,23 +13,30 @@ class Resume extends Component {
   render() {
     if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      // var education = this.props.data.education.map(function(education){
-      //   return <div key={education.school}><h3>{education.school}</h3>
-      //   <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-      //   <p>{education.description}</p></div>
-      // })
-      // var work = this.props.data.work.map(function (work) {
-      //   return (
-      //     <div key={work.company}>
-      //       <h3>{work.company}</h3>
-      //       <p className="info">
-      //         {work.title}
-      //         <span>&bull;</span> <em className="date">{work.years}</em>
-      //       </p>
-      //       <p>{work.description}</p>
-      //     </div>
-      //   );
-      // });
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">
+              {education.degree} <span>&bull;</span>
+              <em className="date">{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
+          </div>
+        );
+      });
+      var work = this.props.data.work.map(function (work) {
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
+            <p className="info">
+              {work.title}
+              <span>&bull;</span> <em className="date">{work.years}</em>
+            </p>
+            <p>{work.description}</p>
+          </div>
+        );
+      });
 
       var skills = this.props.data.skills.map((skills) => {
         var className = "bar-expand " + skills.name.toLowerCase();
@@ -50,21 +57,21 @@ class Resume extends Component {
 
     return (
       <section id="resume">
-        {/* <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">{education}</div>
             </div>
-         </div>
-      </div> */}
+          </div>
+        </div>
 
-        {/* <div className="row work">
+        <div className="row work">
           <div className="three columns header-col">
             <h1>
               <span>Work</span>
@@ -72,7 +79,7 @@ class Resume extends Component {
           </div>
 
           <div className="nine columns main-col">{work}</div>
-        </div> */}
+        </div>
 
         <div className="row skill">
           <div className="three columns header-col">
