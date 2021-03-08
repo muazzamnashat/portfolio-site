3d32b9c45b2def24b3514c543dd535b9436d7f03
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-03-07 22:35:10 -0500

    move resume pdf to src, add resume link, phone and email to About component and use object destructure to assign var from props

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react';
-
-class About extends Component {
-  render() {
-
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
-    }
-
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
-         </div>
-      </div>
-
-   </section>
-    );
+import React, { useState } from "react";
+import pdf from "../resume.pdf";
+function About(props) {
+  if (props.data) {
+    var {
+      name,
+      image,
+      bio,
+      street,
+      city,
+      state,
+      zip,
+      phone,
+      email,
+      resumeDownload,
+    } = props.data;
   }
+
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img
+            className="profile-pic"
+            src={`images/${image}`}
+            alt="Muazzam Nashat profile pic"
+          />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+
+          <p>{bio}</p>
+          <div className="row">
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>{name}</span>
+                <br />
+                <a href={`tel:${phone}`}>Call me at {phone}</a>
+                <br />
+                <a href={`mailto:${email}`}>{email}</a>
+              </p>
+            </div>
+            <div className="columns download">
+              <p>
+                <a href={pdf} target="_blank" className="button">
+                  <i className="fa fa-download"></i>Download Resume
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
