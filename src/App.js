d00abb46f837ac30961890f280949001ccd64319
muazzamnashat <muazzamhc.nashat@gmail.com>
2021-04-04 23:50:55 -0400

    add resume pdf

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
-import ReactGA from "react-ga";
-import $ from "jquery";
-import "./App.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Portfolio from "./Components/Portfolio";
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      foo: "bar",
+      foo: 'bar',
       resumeData: {},
     };
 
-    ReactGA.initialize("UA-110570651-1");
+    ReactGA.initialize('UA-110570651-1');
     ReactGA.pageview(window.location.pathname);
   }
 
   getResumeData() {
     $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
+      url: './resumeData.json',
+      dataType: 'json',
       cache: false,
       success: function (data) {
         this.setState({ resumeData: data });
@@ -42,10 +42,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
+        {/* <Resume data={this.state.resumeData.resume} /> */}
         <Portfolio data={this.state.resumeData.portfolio} />
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
